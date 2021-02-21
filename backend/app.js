d694083a7061538d2a4a64263d3d93eb4e42a8d5
cin-vong <61393238+cin-vong@users.noreply.github.com>
2021-02-21 12:04:02 +0100

    commit-00

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const connection = require('./dbParams');
+const app = express();
+const bodyParser = require('body-parser');
+
+//Routes
+const userRoutes = require('./routes/user');
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  //Connexion DB
+
+connection.connect(function(err){
+  if (err){
+    console.log("Erreur de connection à la BDD " + err)
+  }
+  else {
+      console.log("Connection BDD Groupomania réussie")
+  }
+});
+
+//Middleware
+
+app.use(bodyParser.json());
+
+//Routes API
+app.use('/api/user', userRoutes);
+
+module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,102 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
+const dbParams = require('../dbParams');
+
+exports.signup = (req, res, next) => {
+  const pool = mysql.createPool({
+      connectionLimit: 10,
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'groupomania'
+  });
+  pool.getConnection(function (err, connection){
+      if (err){
+          console.log(err)
+          return res.status(400).json("Erreur interne")
+      }
+      const user = req.body
+      bcrypt.hash(user.password, 10) 
+      .then((hash) => {
+          user.password = hash;
+          connection.query('SELECT * from users WHERE email=?', user.email, (err, result) => {
+              if (err) {
+                  console.log(err)
+                  return res.status(400).json("Erreur interne")
+              }
+              if(result.length >= 1) {
+                  return res.status(500).json({ message: "Email déjà existante."});
+              } else {
+                  connection.query(`INSERT INTO users SET ?`, user, (err, result) => {
+                      if (err) {
+                          console.log(err)
+                          return res.status(400).json("Erreur interne")
+                      }
+                  return res.status(201).json({message : 'Compte déjà crée !'})
+                  });
+                  connection.release();
+              }
+          })
+      }); 
+  }); 
+}
+
+exports.login = (req, res, next) => {
+  const user = req.body
+  if (user.email && user.password){
+    dbParams.query('SELECT * FROM users WHERE email = ?', user.email, function(err, results){
+          if (err){
+              console.log(err)
+              return res.status(400).json("Erreur interne")
+          }
+          if (results.length <= 0){
+              return res.status(500).json({ message: "Email inconnu"})
+          } else {
+              bcrypt.compare(user.password, results[0].password)
+              .then(valid => {
+                  if(!valid){
+                      return res.status(500).json({ message: "Email ou mot de passe incorrect"});
+                  } else {
+                      return res.status(200).json({
+                          id: results[0].id,
+                          username: results[0].username,
+                          email: results[0].email,
+                          roleAdmin: results[0].roleAdmin,
+                          token: jwt.sign(
+                          { id: results[0].id},
+                          process.env.TOKEN,
+                          { expiresIn : '24h'}
+                          ) 
+                      })
+                  }
+              })
+              .catch(() => {
+                  return res.status(500).json({ message : 'Erreur interne' })
+              })
+          }
+      })
+  }
+}
+
+exports.getOneUser = (req, res, next) => {
+  const userId = req.params.id
+  dbParams.query('SELECT * FROM users WHERE id="'+userId+'"', function(err,result){
+    if (err){
+        console.log(err);
+        return res.status(400).json({ message : "Erreur interne" })
+    }
+    return res.status(201).json({ result })
+})
+};
+
+exports.deleteUser = (req, res, next) => {
+  const userId = req.params.id
+  dbParams.query('DELETE FROM users WHERE id=?', userId, function(err,result){
+      if (err){
+          console.log(err);
+          return res.status(400).json({ message : "Erreur interne" })
+      }
+      return res.status(201).json({ message : "Utilisateur supprimé."})
+  })
+};
