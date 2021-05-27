b0713cc768f1be1b559122d312ce58f9eb576b57
cin-vong <61393238+cin-vong@users.noreply.github.com>
2021-05-27 19:30:52 +0200

    commit-18

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -2,7 +2,7 @@ const dbParams = require("../database_connect");
 const fs = require("fs");
 // All post
 exports.getAllPost = (req, res, next) => {
-    dbParams.query('SELECT user.nom, user.prenom, posts.id, posts.userId, posts.title, posts.content, posts.date AS date FROM user INNER JOIN posts ON user.id = posts.userId ORDER BY date DESC', (error, result, field) => {
+    dbParams.query('SELECT user.nom, user.prenom, posts.id, posts.userId, posts.title, posts.content, posts.image, posts.date AS date FROM user INNER JOIN posts ON user.id = posts.userId ORDER BY date DESC', (error, result, field) => {
         if (error) {
             return res.status(400).json({
                 error
@@ -13,18 +13,21 @@ exports.getAllPost = (req, res, next) => {
 };
 // NewPost
 exports.newPost = (req, res, next) => {
-    const filename = req.file.filename;
+    const filename = req.file;
+    if(req.file && req.file.filename) {
+     filename = req.file.filename
+    }
     console.log(req.file);
-     req.body.data = JSON.parse(req.body.data)//transforme le json en object 
+    console.log(req.body);
+    // req.body.data = JSON.parse(req.body.data)//transforme le json en object 
      const thepost = {
-         userId: req.body.data.userId,
-         title: req.body.data.title,
-         content: req.body.data.content,
-         attachmentUrl: filename`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-         //attachmentUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
+         userId: req.body.userId,
+         title: req.body.title,
+         content: req.body.content,
+         image: filename`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
      }
         
-    dbParams.query('INSERT INTO posts (userId, title, content, attachmentUrl) VALUE(?,?,?)', [userId, title, content, attachmentUrl], thepost, (error, result) => {
+    dbParams.query('INSERT INTO posts (userId, title, content, image) VALUE(?,?,?)', [userId, title, content, image], thepost, (error, result) => {
         if (error) {
             console.log(error)
             return res.status(400).json ({ message: "Erreur interne" })
@@ -49,7 +52,7 @@ exports.deleteOnePost = (req, res, next) => {
     let sqlDeletePost;
     dbParams.query(sqlSelectPost, [posts.id], function (err, result) {
     if (result > 0) {
-        const filename = result[0].attachmentUrl.split("/images/")[1];
+        const filename = result[0].image.split("/images/")[1];
         fs.unlink(`images/${filename}`, () => { // On supprime le fichier image 
             sqlDeletePost = "DELETE FROM Post WHERE userId = ? AND posts.id  = ?"
             });
--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -3,18 +3,25 @@ const multer = require('multer');
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
-  'image/png': 'png'
+  'image/png': 'png',
+  'image/gif': 'gif'
 };
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
   },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
+  filename: function (req, file, cb) {
+    if (file == undefined) {
+      cb();
+    }
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    let filename = Date.now() +
+      "." +
+      extension;
+    cb(null, filename);
   }
+
 });
 
 module.exports = multer({ storage: storage }).single('image');
