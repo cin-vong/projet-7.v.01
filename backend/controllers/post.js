4b6ed838f82cdc1df2da7a7f98a6025fa6cfbbf1
cin-vong <61393238+cin-vong@users.noreply.github.com>
2021-06-27 10:27:56 +0200

    commit-23

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,6 +1,8 @@
 const dbParams = require("../database_connect");
 const fs = require("fs");
 
+//----------------------POSTS------------------------------------
+
 // All post
 exports.getAllPost = (req, res, next) => {
     dbParams.query('SELECT user.nom, user.prenom, posts.id, posts.userId, posts.title, posts.content, posts.image, posts.date AS date FROM user INNER JOIN posts ON user.id = posts.userId ORDER BY date DESC', (error, result, field) => {
@@ -12,6 +14,19 @@ exports.getAllPost = (req, res, next) => {
         return res.status(200).json(result);
     });
 };
+
+// OnePost
+exports.getOnePost = (req, res, next) => {
+    dbParams.query(`SELECT * FROM posts WHERE posts.id = ${req.params.id}`, (error, result, field) => {
+        if (error) {
+            return res.status(400).json({
+                error
+            });
+        }
+        return res.status(200).json(result);
+    });
+};
+
 // NewPost
 exports.newPost = (req, res, next) => {
      const thepost = {
@@ -28,54 +43,39 @@ exports.newPost = (req, res, next) => {
         return res.status(201).json({ message: ' Votre message a bien été posté !'})
     })
 };
-// OnePost
-exports.getOnePost = (req, res, next) => {
-    dbParams.query(`SELECT * FROM posts WHERE posts.id = ${req.params.id}`, (error, result, field) => {
-        if (error) {
-            return res.status(400).json({
-                error
-            });
-        }
-        return res.status(200).json(result);
-    });
-};
+
+
 // Delete OnePost
-exports.deleteOnePost = (req, res, next) => {
-    let imageFilename = req.file;;
-    dbParams.query('SELECT * FROM posts WHERE posts.id = ? ', [posts.id], function (err, result) {
-    if (result > 0) {
-        const filename = result[0].image.split("/images/")[1];
-        fs.unlink(`images/${filename}`, () => { // On supprime le fichier image 
-            imageFilename = "DELETE FROM Post WHERE userId = ? AND posts.id  = ?"
-            });
+exports.deleteOnePost = (req, res, next) => { 
+    const postId = req.params.id;
+    dbParams.query('SELECT * FROM posts Where id=?', postId, (err, res) => {
+        if (err) {
+            console.log(err)
+            return res.status(400).json("Erreur interne")
+        } else {
+            const imageResult = res;
+            const filename = imageResult[0].image
+            fs.unlink(`images/${filename}`,() => {
+                dbParams.query('DELETE FROM posts WHERE id=?', postId, (error, res) => {
+                    if(error){
+                        console.log(error);
+                        return res.status(400).json({ message : "Erreur interne "})
+                    }
+                })
+            })
         }
-    dbParams.query(`DELETE FROM posts WHERE posts.id = ${req.params.id}`, (error, result, field) => {
-        if (error) {
-            return res.status(400).json({
-                error
-            });
-        }
-        return res.status(200).json(result);
-    });
     })
-};
+}
+   
 // Modify OnePost
 exports.modifyOnePost = (req, res, next) => {
-    const filename = req.file;
-    console.log("ICI", filename);
-    if(req.file !=null && req.file.filename !=null) {
-     filename = req.file.filename
+    const modifiyPost = {
+        userId: req.body.userId,
+        title: req.body.title,
+        content: req.body.content,
+        image:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     }
-    console.log(req.file);
-    console.log(req.body);
-    // req.body.data = JSON.parse(req.body.data)//transforme le json en object 
-     const Modifydata = {
-         userId: req.body.userId,
-         title: req.body.title,
-         content: req.body.content,
-         image: filename`${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-     }
-    dbParams.query(`UPDATE posts SET title = '${req.body.title}', content = '${req.body.content}' WHERE posts.id = ${req.params.id}`, Modifydata, (error, result, field) => {
+    dbParams.query(`UPDATE posts SET title = '${req.body.title}', content = '${req.body.content}' WHERE posts.id = ${req.params.id}`, modifiyPost, (error, result, field) => {
         if (error) {
             return res.status(400).json({
                 error
@@ -84,6 +84,7 @@ exports.modifyOnePost = (req, res, next) => {
         return res.status(200).json(result);
     });
 };
+
 // Get User's Posts
 exports.getUserPosts = (req, res, next) => {
     dbParams.query(`SELECT * FROM posts WHERE posts.userId = ${req.params.id}`, (error, result, field) => {
@@ -95,6 +96,9 @@ exports.getUserPosts = (req, res, next) => {
         return res.status(200).json(result);
     });
 };
+
+//----------------------COMMENTS------------------------------------
+
 // New comment
 exports.newComment = (req, res, next) => {
     dbParams.query(`INSERT INTO comments VALUES (NULL, ${req.body.userId}, ${req.params.id}, NOW(), '${req.body.content}')`, (error, result, field) => {
@@ -106,6 +110,7 @@ exports.newComment = (req, res, next) => {
         return res.status(200).json(result);
     });
 };
+
 // Get all comments
 exports.getAllComments = (req, res, next) => {
     dbParams.query(`SELECT user.id, user.nom, user.prenom, comments.id,comments.content, comments.userId, comments.date FROM user INNER JOIN comments ON user.id = comments.userId WHERE comments.postId = ${req.params.id} ORDER BY comments.date DESC`,
@@ -118,6 +123,7 @@ exports.getAllComments = (req, res, next) => {
             return res.status(200).json(result);
         });
 };
+
 //Delete comment
 exports.deleteComment = (req, res, next) => {
     dbParams.query(`DELETE FROM comments WHERE comments.id = ${req.params.id}`, (error, result, field) => {
